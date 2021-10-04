98620c17d62de956710135b34d8176f8b41ffe05
Christopher M. Judd <javajudd@gmail.com>
2021-10-04 11:05:19 -0400

    Initial creation of Modern Java Unit Testing and Mocking Workshop created using https://start.spring.io/ and no dependencies.

---
+++ src/main/java/net/javajudd/modernjavaunittesting/ModernJavaUnitTestingApplication.java
@@ -0,0 +1,13 @@
+package net.javajudd.modernjavaunittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJavaUnitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernJavaUnitTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javajudd/modernjavaunittesting/ModernJavaUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javajudd.modernjavaunittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaUnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
