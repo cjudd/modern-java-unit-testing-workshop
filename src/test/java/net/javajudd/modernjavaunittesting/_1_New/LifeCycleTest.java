39ca79409b43fe612f4d05f502c4021700e8cd94
Christopher M. Judd <javajudd@gmail.com>
2021-10-04 16:33:41 -0400

    Added additional documentation.

--- src/test/java/net/javajudd/modernjavaunittesting/_1_New/LifeCycleTest.java
+++ src/test/java/net/javajudd/modernjavaunittesting/_1_New/LifeCycleTest.java
@@ -1,8 +1,10 @@
 package net.javajudd.modernjavaunittesting._1_New;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class LifeCycleTest {
+
+    // Run tests to see the current output.
 
     // TODO 1.0 - Write equivalent of @BeforeClass printing "Before Everything".
     // TODO 1.1 - Write equivalent of @AfterClass printing "After Everything".
@@ -10,7 +12,7 @@ public class LifeCycleTest {
     // TODO 1.2 - Write equivalent of @Before printing "Before Test".
     // TODO 1.3 - Write equivalent of @After printing "After Test".
 
-    // Run tests to make sure you understand the lifecycle.
+    // Run again tests to make sure you understand the lifecycle.
 
     @Test
     void one() {
